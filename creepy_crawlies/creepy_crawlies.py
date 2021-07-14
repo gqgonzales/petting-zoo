2906fe5a04e4ec105d6730a848598878565fc384
Gabe Gonzales <gq.gonzales1@gmail.com>
2021-07-14 14:07:43 -0500

    Ch 6 complete, Animals can now eat!

--- creepy_crawlies/creepy_crawlies.py
+++ creepy_crawlies/creepy_crawlies.py
@@ -4,62 +4,92 @@ from datetime import date
 
 
 class Coral_Snake:
-
-    def __init__(self, name, species):
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species, food):
+        self.name = name
+        self.species = species
         self.date_added = date.today()
-        self.swimming = False
-        self.slithering = True
-        self.walpiper = False
+        self.swimming = True
+        self.slithering = False
+        self.walking = False
+        self.food = food
+
+    def __str__(self):
+        return f"{self.name} is a {self.species}"
+
+    def feed(self):
+        print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
 
 
 class Garden_Snake:
-
-    def __init__(self, name, species):
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species, food):
+        self.name = name
+        self.species = species
         self.date_added = date.today()
-        self.swimming = False
-        self.slithering = True
-        self.walpiper = False
+        self.swimming = True
+        self.slithering = False
+        self.walking = False
+        self.food = food
+
+    def __str__(self):
+        return f"{self.name} is a {self.species}"
+
+    def feed(self):
+        print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
 
 
 class Copperhead:
-
-    def __init__(self, name, species):
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species, food):
+        self.name = name
+        self.species = species
         self.date_added = date.today()
-        self.swimming = False
-        self.slithering = True
-        self.walpiper = False
+        self.swimming = True
+        self.slithering = False
+        self.walking = False
+        self.food = food
+
+    def __str__(self):
+        return f"{self.name} is a {self.species}"
+
+    def feed(self):
+        print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
 
 
 class Cobra:
-
-    def __init__(self, name, species):
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species, food):
+        self.name = name
+        self.species = species
         self.date_added = date.today()
-        self.swimming = False
-        self.slithering = True
-        self.walpiper = False
+        self.swimming = True
+        self.slithering = False
+        self.walking = False
+        self.food = food
+
+    def __str__(self):
+        return f"{self.name} is a {self.species}"
+
+    def feed(self):
+        print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
 
 
 class Diamondback:
-
-    def __init__(self, name, species):
-        self.name = ""
-        self.species = ""
+    def __init__(self, name, species, food):
+        self.name = name
+        self.species = species
         self.date_added = date.today()
-        self.swimming = False
-        self.slithering = True
-        self.walpiper = False
+        self.swimming = True
+        self.slithering = False
+        self.walking = False
+        self.food = food
+
+    def __str__(self):
+        return f"{self.name} is a {self.species}"
+
+    def feed(self):
+        print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
 
 
-# king = Coral_Snake("King", "coral snake")
-# kevin = Garden_Snake("Kevin", "garden snake")
-# jewel = Copperhead("Jewel", "pit viper")
-# nikki = Cobra("Nikki", "king cobra")
-# flora = Diamondback("Flora", "western diamondback rattlesnake")
+# king = Coral_Snake("King", "coral snake", "field mice")
+# kevin = Garden_Snake("Kevin", "garden snake", "little lizards")
+# jewel = Copperhead("Jewel", "pit viper", "whatever she wants")
+# nikki = Cobra("Nikki", "king cobra, "field mice"")
+# flora = Diamondback("Flora", "western diamondback rattlesnake", "sour patch kids")
--- landlubbers/landlubbers.py
+++ landlubbers/landlubbers.py
@@ -4,67 +4,103 @@ from datetime import date
 
 
 class Llama:
-    def __init__(self, name, species, shift):
+    def __init__(self, name, species, shift, food):
         # Establish the properties of each animal
         # with a default value
-        self.name = ""
-        self.species = ""
-        self.shift = ""
+        self.name = name
+        self.species = species
+        self.shift = shift
         self.date_added = date.today()
         self.swimming = False
         self.slithering = False
-        self.walpiper = True
+        self.walking = True
+        self.food = food
+
+    def __str__(self):
+        return f"{self.name} is a {self.species}"
+
+    def feed(self):
+        print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
 
 
 class Donkey:
-    def __init__(self, name, species, shift):
-        self.name = ""
-        self.species = ""
-        self.shift = ""
+    def __init__(self, name, species, shift, food):
+        self.name = name
+        self.species = species
+        self.shift = shift
         self.date_added = date.today()
         self.swimming = False
         self.slithering = False
-        self.walpiper = True
+        self.walking = True
+        self.food = food
+
+    def __str__(self):
+        return f"{self.name} is a {self.species}"
+
+    def feed(self):
+        print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
 
 
 class Goat:
-    def __init__(self, name, species, shift):
-        self.name = ""
-        self.species = ""
-        self.shift = ""
+    def __init__(self, name, species, shift, food):
+        self.name = name
+        self.species = species
+        self.shift = shift
         self.date_added = date.today()
         self.swimming = False
         self.slithering = False
-        self.walpiper = True
+        self.walking = True
+        self.food = food
+
+    def __str__(self):
+        return f"{self.name} is a {self.species}"
+
+    def feed(self):
+        print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
 
 
 class Cow:
-    def __init__(self, name, species, shift):
-        self.name = ""
-        self.species = ""
-        self.shift = ""
+    def __init__(self, name, species, shift, food):
+        self.name = name
+        self.species = species
+        self.shift = shift
         self.date_added = date.today()
         self.swimming = False
         self.slithering = False
-        self.walpiper = True
+        self.walking = True
+        self.food = food
+
+    def __str__(self):
+        return f"{self.name} is a {self.species}"
+
+    def feed(self):
+        print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
 
 
 class Horse:
-    def __init__(self, name, species, shift):
-        self.name = ""
-        self.species = ""
-        self.shift = ""
+    def __init__(self, name, species, shift, food):
+        self.name = name
+        self.species = species
+        self.shift = shift
         self.date_added = date.today()
         self.swimming = False
         self.slithering = False
-        self.walpiper = True
+        self.walking = True
+        self.food = food
+
+    def __str__(self):
+        return f"{self.name} is a {self.species}"
+
+    def feed(self):
+        print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
 
 
-# miss_fuzz = Llama("Miss Fuzz", "domestic llama", "morning")
-# eeyore = Donkey("Eeyore", "blue donkey", "afternoon")
-betsy = Cow("Betsy", "cow", "midday")
-# lats = Goat("Lats", "shorthair mountain goat", "afternoon")
-# legolas = Horse("Legolas", "black horse", "midday")
-print(
-    f"{betsy.name} the {betsy.species} is available to pet during the {betsy.shift} shift."
-)
+# miss_fuzz = Llama("Miss Fuzz", "domestic llama", "morning", "Llama Chow")
+# eeyore = Donkey("Eeyore", "blue donkey", "afternoon", "cantaloupe")
+# betsy = Cow("Betsy", "cow", "midday", "grass")
+# lats = Goat("Lats", "shorthair mountain goat", "afternoon", "tin cans")
+# legolas = Horse("Legolas", "black horse", "midday", "honeydew")
+# print(
+#     f"{miss_fuzz.name} the {miss_fuzz.species} is available to pet during the {miss_fuzz.shift} shift."
+# )
+# print(miss_fuzz.feed())
